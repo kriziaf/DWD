a31fd5c170398ddc711f70204aa2580d23e4025a
krizia <kriziafernando@gmail.com>
2018-02-02 13:10:05 -0500

    Class 2 files

---
+++ WEEK2/server.js
@@ -0,0 +1,1 @@
+console.log("I have a gnu server");
--- WEEK2/webserver.js
+++ WEEK2/webserver.js
@@ -1,11 +1,35 @@
-// this is a webserver
-
 var express = require('express')
 var app = express()
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+var count = 0;
+
+var submissions = [];
+
+app.get('/somethingelse', function (req, res) {
+	count++;
+  res.send('<html><body><h1>Something Else' + count + '</h1></body></html>')
+})
+
+app.get('/formpost', function (req, res) {
+	console.log("They submitted: " + req.query.textfield);
+	res.send("You submitted: " + req.query.textfield);
+	submissions.push(req.query.textfield);
+})
+
+app.get('/display', function(req, res) {
+	var html = "<html><body>";
+	for (var i = 0; i < submissions.length; i++) {
+		html = html + submissions[i] + "<br>";
+	}
+	html = html + "</body></html>";
+	res.send(html);
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
