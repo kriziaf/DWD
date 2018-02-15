1dfc47b9498d47b77beaf454a7d33104ad7c1dec
krizia <kriziafernando@gmail.com>
2018-02-15 17:16:21 -0500

    clean up code

--- WEEK3/public/js/app.js
+++ WEEK3/public/js/app.js
@@ -1,55 +1,18 @@
-$(function() {
+$("#add").on("click", function(){
 
+  // add new entry
+  let newTodo = $("#entry").val();
+  $("#entry").val("");
+  $("#lineEntry").append("<li>"+newTodo+"</li>");
 
-	var task = 0;
-	//listen for add button click
-	$('#add').click(addTask);
+  // update count:
+  let prevCount = Number($("#num").text());
+  $("#num").text(  prevCount + 1   );
 
+  // send ajax request with the newTodo item to
+  // our server so we can put it into the database
+  $.get("/new?val="+newTodo, function(a){
+    console.log("worked! ", a);
+  })
 
-		function addTask(e){
-			e.preventDefault();
-
-			//get content in the field
-			var entry = $('#entry').val();
-
-			//make new list item with the content
-			entry = '<li>'+ entry + '</li>';
-
-			//display entry
-			$('#lineEntry').append(entry);
-
-			if (isNaN(entry)) {
-     		entry = "write something";
-   			}
-
-			//add 1 to task, then display
-			task ++;
-			$('#num').text(task);
-
-			//clear the field
-			$('#entry').val("");
-
-		}
-
-			//complete task
-			$('body').on('click', '#lineEntry li', function(){
-
-    			/*alert($(this).text());*/
-    			$(this).toggleClass("done");
-
-    			//update status
-    			if ($(this).hasClass('done')) {
-    				task --;
-
-    			} else {
-	    			task ++;
-	    			$('#num').text(task);
-    			}
-
- 			});
-
-
-			//edit task
-
-
-});
+})
--- WEEK3/webserver.js
+++ WEEK3/webserver.js
@@ -1,82 +1,32 @@
-// this is a webserver
-
-var config = require("./config.js");
-
-var mongojs = require("mongojs");
-//****define the mongo DB
-//mongojs("username:password@example.com:port/mydb", ["submissions"]);
-
-var db = mongojs(config.username+":"+config.password+"+@ds229878.mlab.com:29878/test_krizia", ["submissions"]);
-
-// var bodyParser = require('body-parser');
-// var urlencodedParser = bodyParser.urlencoded({ extended: true }); // for parsing form data
-// app.use(urlencodedParser);
-
-//use express
-
+// import express server stuff
 var express = require('express')
+// initialize the express stuff
 var app = express()
-//app.use(express.static('public'));
+// tell express that if there is no specific route
+// just check if the file are in public
+// the "/" router defaults to "/index.html"
 
-//use template.ejs
+// let express know that sometimes we will
+// use ejs templates from the view folder
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-var count = 0;
-var submissions = [];
-//define a route named somethingelse
-//GET REQUEST
-// var count = 0;
-// var submissions = [];
-app.get('/somethingelse', function (req, res) {
-	count++;
-  res.send('<html><body><h1>Something Else' + count + '</h1></body></html>')
-})
-
-//define route as action=formpost
-//GET REQUEST
-  // app.get('/formpost', function (req, res) {
-  // 	res.send("You submitted: " + req.query.textfield);
-  // 	submissions.push(req.query.textfield);
-  // })
-
-app.get('/formpost', function (req, res) {
-  //console.log("THIS HAS BEEN ADDED BY SOME TO DO LISTER: ", req.query.val);
-  let userinput = req.query.task;
-  // here you probably want to put this into mongoDB
-  res.send("You submitted: " + userinput);
-	submissions.push(req.query.task);
-
-})
-
-app.get('/test',function(req,res){
-  // var value = {submissions: {lineEntry:"data"}};
-  var value = [ {lineEntry:req.query.task},{lineEntry:"brezl"},{lineEntry:"jim"}];
-  res.render('template.ejs',{'data': value});
+let testData = [{lineEntry:"brezl"},{lineEntry:"jim"},{lineEntry:"gym"}];
+app.get('/',function(req,res){
+  // pull from db
+  let value = {data:{lineEntry:req.query.task}};
+  // and what comes back should be in the
+  // testData object (atm this is just test data)
+  // res.render('template.ejs',{"data": testData});
+  res.render('template.ejs',{"data": value});
 });
-  //ejs template
-app.get('/display',function(req,res){
-  //ERROR HAPPENS HERE
-  //{"text":"req.query.textfield"}
-  var value = {data:{lineEntry:req.query.task}};
-  res.render('template.ejs',value);
 
-  //****mongo.save
-  //app.post('/display')
-  //req.body.task
-  //post route body parser
-
-  //app.get MISSING HERE??
-  db.submissions.save({"submissions":value}, function(err, saved) {
-    if( err || !saved ) console.log("Not saved");
-    else console.log("Saved!");
-  });
-  // app.get('/display', function(req, res) {
-  // //*****mongo.find()
+app.get('/new',function(req,res){
+  let newTodo = req.query.val
+  // add to db
 });
+
+app.use(express.static('public'));
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
