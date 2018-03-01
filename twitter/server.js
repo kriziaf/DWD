494258d9e5373d9d1451484a19f5345f18ea10a3
krizia <kriziafernando@gmail.com>
2018-03-01 14:49:57 -0500

    updated twitter

--- twitter/server.js
+++ twitter/server.js
@@ -1,3 +1,5 @@
+var config = require("./config.js")
+
 var Twitter = require('twitter-node-client').Twitter;
 const express = require('express')
 const app = express()
@@ -14,13 +16,13 @@ const app = express()
 	var Twitter = require('twitter-node-client').Twitter;
 
 	//Get this data from your twitter apps dashboard
-	var config = {
-    	"consumerKey":"ieqO6HaEoLmzYBdO3m40tMzia",
-    	"consumerSecret":"vHoL1OWwYS8REgjLseSDNTrbmzPgMeYbZ6UJUAKw22HJlUTfcI",
-    	"accessToken":"959537297900359681-eNeKKaFn3PuhOW5AennXfBHTM8ukkKc",
-    	"accessTokenSecret":"suVMCOm7CUNY9658WnJsWEcYUPqecbjaC2OLJqatujFe6",
-    	"callBackUrl": ""
-	}
+	// var config = {
+  //   	"consumerKey":"ieqO6HaEoLmzYBdO3m40tMzia",
+  //   	"consumerSecret":"vHoL1OWwYS8REgjLseSDNTrbmzPgMeYbZ6UJUAKw22HJlUTfcI",
+  //   	"accessToken":"959537297900359681-eNeKKaFn3PuhOW5AennXfBHTM8ukkKc",
+  //   	"accessTokenSecret":"suVMCOm7CUNY9658WnJsWEcYUPqecbjaC2OLJqatujFe6",
+  //   	"callBackUrl": ""
+	// }
 
 	// make a directory in the root folder of your project called data
 	// copy the node_modules/twitter-node-client/twitter_config file over into data/twitter_config`
